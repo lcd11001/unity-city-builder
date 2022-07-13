2f667f03c1b8f27efffe4ba274c0734123e62e59
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-07-13 11:32:34 +0700

    + Building resource

--- Assets/Scripts/BuildingObject.cs
+++ Assets/Scripts/BuildingObject.cs
@@ -5,4 +5,65 @@ using UnityEngine;
 public class BuildingObject : MonoBehaviour
 {
     public Building data;
+
+    [Header("Resource generation")]
+    [Space(8)]
+    // this will be the resource that has been created by this building
+    public float resource = 0;
+
+    // limit that this building can generate or do
+    public float resourceLimit = 100;
+
+    // speed that the resource is generated
+    public float generationSpeed = 5;
+
+    Coroutine buildingBehaviour;
+
+    private void Start()
+    {
+        buildingBehaviour = StartCoroutine(CreateResource());
+    }
+
+    private void OnMouseDown()
+    {
+        bool usedResource = false;
+
+        switch (data.resourceType)
+        {
+            case Building.ResourceType.Standard:
+                usedResource = ResourceManager.Instance.AddStandardC((int)resource);
+                break;
+            case Building.ResourceType.Premium:
+                usedResource = ResourceManager.Instance.AddPremiumC((int)resource);
+                break;
+        }
+
+        if (usedResource)
+        {
+            EmptyResource();
+        }
+    }
+
+    void EmptyResource()
+    {
+        resource = 0;
+    }
+
+    IEnumerator CreateResource()
+    {
+        // It will create resource infinitely
+        while (true)
+        {
+            if (resource < resourceLimit)
+            {
+                resource += generationSpeed * Time.deltaTime;
+            }
+            else
+            {
+                resource = resourceLimit;
+            }
+
+            yield return null;
+        }
+    }
 }
